108863e72e8d408d3f7d9cb3b59d13b35e88137c
RhodyArthur <rhodaarthur48@gmail.com>
2023-04-29 14:36:12 -0700

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,31 +9,31 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *tmp;
+	listint_t *curr, *tmp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    curr = (*list)->next;
+	curr = (*list)->next;
 
-    while (curr != NULL)
-    {
-        tmp = curr->prev;
-        while (tmp != NULL && tmp->n > curr->n)
-        {
-            tmp->next = curr->next;
-            if (tmp->next != NULL)
-                tmp->next->prev = tmp;
-            curr->next = tmp;
-            curr->prev = tmp->prev;
-            if (tmp->prev != NULL)
-                tmp->prev->next = curr;
-            tmp->prev = curr;
-            tmp = curr->prev;
-            if (tmp == NULL)
-                *list = curr;
-            print_list(*list);
-        }
-        curr = curr->next;
-    }
+	while (curr != NULL)
+	{
+		tmp = curr->prev;
+		while (tmp != NULL && tmp->n > curr->n)
+		{
+			tmp->next = curr->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp;
+				curr->next = tmp;
+				curr->prev = tmp->prev;
+			if (tmp->prev != NULL)
+				tmp->prev->next = curr;
+				tmp->prev = curr;
+				tmp = curr->prev;
+			if (tmp == NULL)
+				*list = curr;
+				print_list(*list);
+		}
+		curr = curr->next;
+	}
 }
