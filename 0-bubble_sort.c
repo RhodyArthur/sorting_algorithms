492c856af4ef9b7c66e1fa3842e369c5a816d964
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-04-29 12:42:35 +0000

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,11 +14,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (size == 0)
+	if (size < 0)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
