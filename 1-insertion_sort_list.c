20dc7f22b37da0e728ab82b752d0d3d366f4d9fb
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-04-29 18:46:12 +0000

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,39 +6,34 @@
  *
  * @list: Pointer to the head of the doubly linked list to be sorted
  */
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *next_node, *temp;
+    listint_t *curr, *tmp;
 
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    current = (*list)->next;
+    curr = (*list)->next;
 
-    while (current != NULL)
+    while (curr != NULL)
     {
-        next_node = current->next;
-
-        while (current->prev != NULL && current->n < current->prev->n)
+        tmp = curr->prev;
+        while (tmp != NULL && tmp->n > curr->n)
         {
-            /* Swap current node with previous node */
-            temp = current->prev;
-            current->prev = temp->prev;
-            temp->next = current->next;
-            current->next = temp;
-
-            if (temp->prev != NULL)
-                temp->prev->next = current;
-            temp->prev = current;
-
-            /* If the swapped node was the head, update the list */
-            if (current->prev == NULL)
-                *list = current;
-
-            /* Print the list after the swap */
+            tmp->next = curr->next;
+            if (tmp->next != NULL)
+                tmp->next->prev = tmp;
+            curr->next = tmp;
+            curr->prev = tmp->prev;
+            if (tmp->prev != NULL)
+                tmp->prev->next = curr;
+            tmp->prev = curr;
+            tmp = curr->prev;
+            if (tmp == NULL)
+                *list = curr;
             print_list(*list);
         }
-
-        current = next_node;
+        curr = curr->next;
     }
 }
